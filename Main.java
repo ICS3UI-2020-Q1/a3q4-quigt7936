b2cdbf6a0ca7528c0e49078301a8b36929149483
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-01 15:52:24 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,49 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program ask the user for a number and tells it in written form
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // Get the number from the user 
+    System.out.println("Please enter a positive integer:");
+    // tell the user their number in written form, or if its not in the range of 1-10 then tell them it is not in the range
+    int userNumber = input.nextInt();
+    switch (userNumber) {
+      case 1:
+        System.out.println("one");
+        break;
+      case 2:
+        System.out.println("two");
+        break;
+      case 3:
+        System.out.println("three");
+        break;
+      case 4:
+        System.out.println("four");
+        break;
+      case 5:
+        System.out.println("five");
+        break;
+      case 6:
+        System.out.println("six");
+        break;
+      case 7:
+        System.out.println("seven");
+        break;
+      case 8:
+        System.out.println("eight");
+        break;
+      case 9:
+        System.out.println("nine");
+        break;
+      case 10:
+        System.out.println("ten");
+        break;
+      default:
+        System.out.println("not in the range");
+        break;
+    }
   }
 }
